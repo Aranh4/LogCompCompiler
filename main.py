10508fdca8eaa1cf5187d29bd9544ab4334a8987
Caio Aranha <caranhatieri@gmail.com>
2024-02-26 18:43:14 -0300

    teste1 mult e div

--- main.py
+++ main.py
@@ -13,55 +13,86 @@ class Tokenizer:
 
     def selectNext(self):
         if self.position >= len(self.source):
-            return Token("EOF", "")
+            self.next = Token("EOF", "")
         else:
             if self.source[self.position] == "+":
                 self.position += 1
-                return Token("PLUS", "+")
-            if self.source[self.position] == "-":
+                self.next = Token("PLUS", "+")
+            elif self.source[self.position] == "-":
                 self.position += 1
-                return Token("MINUS", "-")
-            if self.source[self.position].isdigit():
+                self.next = Token("MINUS", "-")
+            elif self.source[self.position].isdigit():
                 start = self.position
                 while self.position < len(self.source) and self.source[self.position].isdigit():
                     self.position += 1
-                return Token("INT", self.source[start:self.position])
+                self.next = Token("INT", self.source[start:self.position])
+            elif self.source[self.position] == "*":
+                self.position += 1
+                self.next = Token("MULT", "*")
+            elif self.source[self.position] == "/":
+                self.position += 1
+                self.next = Token("DIV", "/")
             elif self.source[self.position] == " ":
                 self.position += 1
-                return self.selectNext()
+                self.selectNext()
         
             
 class Parser:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
-        
+
+
+    def parseTerm (self):
+
+        res = 0
+       
+        if self.tokenizer.next.type == "INT" or self.tokenizer.next.type == "EOF":
+            res = int(self.tokenizer.next.value)
+            next = self.tokenizer.selectNext()
+            while self.tokenizer.next.type == "MULT" or self.tokenizer.next.type == "DIV":
+                if self.tokenizer.next.type == "MULT":
+                    next = self.tokenizer.selectNext()
+                    if self.tokenizer.next.type == "INT":
+                        res *= int(self.tokenizer.next.value)
+                        next = self.tokenizer.selectNext()
+                    else:
+                        sys.stderr.write("token invalido1")
+                elif self.tokenizer.next.type == "DIV":
+                    next = self.tokenizer.selectNext()
+                    if self.tokenizer.next.type == "INT":
+                        res /= int(self.tokenizer.next.value)
+                        next = self.tokenizer.selectNext()
+                    else:
+                        sys.stderr.write("token invalido2")
+          
+        else:
+            sys.stderr.write("token invalido3") 
+       
+        return int(res)
 
     def parseExpression(self):
-        token = self.tokenizer.selectNext()
-        res = 0
-        if token.type == "INT":
-            res = int(token.value)
-            token = self.tokenizer.selectNext()
-            while token.type == "PLUS" or token.type == "MINUS":
-                if token.type == "PLUS":
-                    token = self.tokenizer.selectNext()
-                    if token.type == "INT":
-                        res += int(token.value)
-                        token = self.tokenizer.selectNext()
-                    else:
-                        sys.stderr.write("token invalido")
-                elif token.type == "MINUS":
-                    token = self.tokenizer.selectNext()
-                    if token.type == "INT":
-                        res -= int(token.value)
-                        token = self.tokenizer.selectNext()
-                    else:
-                        sys.stderr.write("token invalido")
-            if token.type != "EOF":
-                sys.stderr.write("token invalido")
+        next = self.tokenizer.selectNext()
+        res = self.parseTerm()
+        while self.tokenizer.next.type == "PLUS" or self.tokenizer.next.type == "MINUS":
+        
+            if self.tokenizer.next.type == "PLUS":
+                next = self.tokenizer.selectNext()
+                if self.tokenizer.next.type == "INT":
+                    res += self.parseTerm()
+                    
+                else:
+                    sys.stderr.write("token invalido4")
+            elif self.tokenizer.next.type == "MINUS":
+                next = self.tokenizer.selectNext()
+                if self.tokenizer.next.type == "INT":
+                    res -= self.parseTerm()
+                 
+                else:
+                    sys.stderr.write("token invalido5")
+        if self.tokenizer.next.type != "EOF":
+            sys.stderr.write("token invalido6")
            
-        else:
-            sys.stderr.write("token invalido") 
+
 
         return res
 
