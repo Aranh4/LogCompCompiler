8e59a6f0ff82baaab0ef16c607badd8641b5ebcb
Caio Aranha <caranhatieri@gmail.com>
2024-03-04 17:09:06 -0300

    roteiro 3

--- main.py
+++ main.py
@@ -32,6 +32,12 @@ class Tokenizer:
             elif self.source[self.position] == "/":
                 self.position += 1
                 self.next = Token("DIV", "/")
+            elif self.source[self.position] == "(":
+                self.position += 1
+                self.next = Token("LPAREN", "(")
+            elif self.source[self.position] == ")":
+                self.position += 1
+                self.next = Token("RPAREN", ")")
             elif self.source[self.position] == " ":
                 self.position += 1
                 self.selectNext()
@@ -41,56 +47,78 @@ class Parser:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
 
-
-    def parseTerm (self):
-
+    @staticmethod
+    def parseFactor(TOKENIZER):
         res = 0
-       
-        if self.tokenizer.next.type == "INT" or self.tokenizer.next.type == "EOF":
-            res = int(self.tokenizer.next.value)
-            next = self.tokenizer.selectNext()
-            while self.tokenizer.next.type == "MULT" or self.tokenizer.next.type == "DIV":
-                if self.tokenizer.next.type == "MULT":
-                    next = self.tokenizer.selectNext()
-                    if self.tokenizer.next.type == "INT":
-                        res *= int(self.tokenizer.next.value)
-                        next = self.tokenizer.selectNext()
-                    else:
-                        sys.stderr.write("token invalido1")
-                elif self.tokenizer.next.type == "DIV":
-                    next = self.tokenizer.selectNext()
-                    if self.tokenizer.next.type == "INT":
-                        res /= int(self.tokenizer.next.value)
-                        next = self.tokenizer.selectNext()
-                    else:
-                        sys.stderr.write("token invalido2")
-          
+        if TOKENIZER.next.type == "INT":
+            res = int(TOKENIZER.next.value)
+            next = TOKENIZER.selectNext()
+        elif TOKENIZER.next.type == "LPAREN":
+            
+            res = Parser.parseExpression(TOKENIZER=TOKENIZER)
+            if TOKENIZER.next.type != "RPAREN":
+
+                sys.stderr.write("token invalido, esperado: RParen, recebido: " + TOKENIZER.next.type + "\n")
+            else:
+                next = TOKENIZER.selectNext()
+  
+        elif TOKENIZER.next.type == "PLUS":
+            next = TOKENIZER.selectNext()
+            res = Parser.parseFactor(TOKENIZER=TOKENIZER)
+        elif TOKENIZER.next.type == "MINUS":
+            next = TOKENIZER.selectNext()
+            res = -Parser.parseFactor(TOKENIZER=TOKENIZER)
         else:
-            sys.stderr.write("token invalido3") 
+            sys.stderr.write("token invalido8")
+        return res
+
+    @staticmethod
+    def parseTerm (TOKENIZER):
+        
+
+        res = Parser.parseFactor(TOKENIZER=TOKENIZER)
        
+        
+        while TOKENIZER.next.type == "MULT" or TOKENIZER.next.type == "DIV":
+            if TOKENIZER.next.type == "MULT":
+                next = TOKENIZER.selectNext()
+                if TOKENIZER.next.type == "INT" or TOKENIZER.next.type == "LPAREN" or TOKENIZER.next.type == "PLUS" or TOKENIZER.next.type == "MINUS":
+                    res *= Parser.parseFactor(TOKENIZER=TOKENIZER)
+                
+                else:
+                    sys.stderr.write("token invalido1")
+            elif TOKENIZER.next.type == "DIV":
+                next = TOKENIZER.selectNext()
+                if TOKENIZER.next.type == "INT" or TOKENIZER.next.type == "LPAREN" or TOKENIZER.next.type == "PLUS" or TOKENIZER.next.type == "MINUS":
+                    res /= Parser.parseFactor(TOKENIZER=TOKENIZER)
+                   
+                else:
+                    sys.stderr.write("token invalido2")
+        
+  
         return int(res)
 
-    def parseExpression(self):
-        next = self.tokenizer.selectNext()
-        res = self.parseTerm()
-        while self.tokenizer.next.type == "PLUS" or self.tokenizer.next.type == "MINUS":
+    @staticmethod
+    def parseExpression(TOKENIZER):
+        next = TOKENIZER.selectNext()
+        res = Parser.parseTerm(TOKENIZER=TOKENIZER)
+        while TOKENIZER.next.type == "PLUS" or TOKENIZER.next.type == "MINUS":
         
-            if self.tokenizer.next.type == "PLUS":
-                next = self.tokenizer.selectNext()
-                if self.tokenizer.next.type == "INT":
-                    res += self.parseTerm()
+            if TOKENIZER.next.type == "PLUS":
+                next = TOKENIZER.selectNext()
+                if TOKENIZER.next.type == "INT" or TOKENIZER.next.type == "LPAREN" or TOKENIZER.next.type == "PLUS" or TOKENIZER.next.type == "MINUS":
+                    res += Parser.parseTerm(TOKENIZER=TOKENIZER)
                     
                 else:
                     sys.stderr.write("token invalido4")
-            elif self.tokenizer.next.type == "MINUS":
-                next = self.tokenizer.selectNext()
-                if self.tokenizer.next.type == "INT":
-                    res -= self.parseTerm()
+            elif TOKENIZER.next.type == "MINUS":
+                next = TOKENIZER.selectNext()
+                if TOKENIZER.next.type == "INT" or TOKENIZER.next.type == "LPAREN" or TOKENIZER.next.type == "PLUS" or TOKENIZER.next.type == "MINUS":
+                    res -= Parser.parseTerm(TOKENIZER=TOKENIZER)
                  
                 else:
                     sys.stderr.write("token invalido5")
-        if self.tokenizer.next.type != "EOF":
-            sys.stderr.write("token invalido6")
+       
            
 
 
@@ -99,7 +127,12 @@ class Parser:
     def run(code):
         tokenizer = Tokenizer(code, 0, None)
         parser = Parser(tokenizer)
-        return parser.parseExpression()
+        result = parser.parseExpression(TOKENIZER=tokenizer)
+        if parser.tokenizer.next.type != "EOF":
+            
+            sys.stderr.write("token invalido, esperado: EOF, recebido: " + parser.tokenizer.next.type + "\n")
+        else:
+            return result
 
             
 def main():
